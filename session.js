e45e8bbc174552c0a9df0de13dcf7e05ff7fd1e3
Osha24 <ffss90062@gmail.com>
2024-11-30 17:25:13 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=nMdUTJyR#4RQem3kz_N__fTZo6UXM1JPOpPZhdBF3rYMsWtkit84",
 };
